bc94858afe0a0bbf5410c1e6adef17709af2f3a6
AjayGhimire <ajayghimire1998@gmail.com>
2022-05-11 21:38:31 +1000

    commit tree bf0e73370b13921462c1d8410cc62b0a54a21032

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,36 @@ class Song
     @@all
   end
 
-  def save
+  def save 
     self.class.all << self
   end
+ 
+  def self.create
+    song = Song.new 
+    song.save
+    song
+  end
 
+  def self.new_by_name name
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name name
+    song = create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name name
+    all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name name
+    find_by_name(name) || create_by_name(name)
+  end
+
+  def alphabetical
 end
+
